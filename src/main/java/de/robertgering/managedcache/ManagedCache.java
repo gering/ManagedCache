495990c67e2d655460b76f8acae8a37d06c748b7
Linda Werner <mail@lindawerner.de>
2012-05-14 19:10:21 +0200

    changed to maven project

--- src/main/java/de/robertgering/managedcache/ManagedCache.java
+++ src/main/java/de/robertgering/managedcache/ManagedCache.java
@@ -1,13 +1,6 @@
 package de.robertgering.managedcache;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  *
